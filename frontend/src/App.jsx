f88820bb6bd0ebdb1b75bda8373a06759d214af2
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-05-13 10:45:50 -0500

    Adding Footer componente and frontend basic layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer';
 
 const App = () => {
     return (
         <div className="App">
             <Header />
+            <section className="content">
+                Contenido
+            </section>
+            <Footer />
         </div>
     );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './styles/Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            Copyright © 2024 Laatcode
+        </footer>
+    )
+}
+
+export default Footer
