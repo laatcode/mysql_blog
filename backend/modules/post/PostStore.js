896ab0cddc18b2858c90b6d3a8ca5232497b3536
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-05-15 12:14:06 -0500

    Implementing order by created_at descending in find posts query on Backend package

--- backend/modules/post/PostStore.js
+++ backend/modules/post/PostStore.js
@@ -6,6 +6,11 @@ class PostStore extends BaseStore {
         super(tableName)
     }
 
+    find() {
+        return this.pool.execute(`SELECT * FROM ${this.table} ORDER BY created_at DESC`)
+            .then(res => res[0])
+    }
+
     create(data) {
         return this.pool.execute(`INSERT INTO ${this.table} (title, content, created_by, created_at, updated_by, updated_at) VALUES (?, ?, ?, NOW(), ?, NOW())`, [data.title, data.content, data.userId, data.userId])
             .then(res => this.findOne(res[0].insertId))
