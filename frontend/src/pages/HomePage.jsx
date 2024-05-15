2e7b54d55cfc570a24a2566ee9e0949b0931db3e
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-05-15 11:59:03 -0500

    Improving styles on frontend package

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
-import { getAllPosts } from "../actions/postActions"
 import Loader from "../components/Loader"
 import PostSummary from "../components/PostSummary"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { getAllPosts } from "../actions/postActions"
 
 import './styles/HomePage.css'
 
@@ -22,12 +25,18 @@ const HomePage = () => {
 
     return (
         <section className="home-page">
-            {loading ? <Loader />
-            : posts.length ?
-            posts.map(post => (
-                <PostSummary key={post.id} post={post} />
-            ))
-            : "No hay post a mostrar"}
+            <Container>
+                <Row>
+                    <Col>
+                        {loading ? <Loader />
+                        : posts.length ?
+                        posts.map(post => (
+                            <PostSummary key={post.id} post={post} />
+                        ))
+                        : "No hay post a mostrar"}
+                    </Col>
+                </Row>
+            </Container>
         </section>
     )
 }
